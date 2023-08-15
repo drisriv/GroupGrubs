3224d3ef4697baf0fb3333f5e12b13d353d58be6
Drishti Srivastava <drishtis@umich.edu>
2023-08-15 13:24:59 -0400

    added text with "lol"

--- GroupGrubs/ContentView.swift
+++ GroupGrubs/ContentView.swift
@@ -14,10 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-            
+            Text("Lol")
         }
         .padding()
-        .padding ()
+        .padding()
         .padding()
     }
 }
