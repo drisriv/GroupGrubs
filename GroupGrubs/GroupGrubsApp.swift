fab308d0f2bf1bd0d356a4f227210b35abe7b933
Drishti Srivastava <drishtis@umich.edu>
2023-08-15 13:05:11 -0400

    Initial Commit

---
+++ GroupGrubs/GroupGrubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupGrubsApp.swift
+//  GroupGrubs
+//
+//  Created by Drishti Srivastava on 8/15/23.
+//
+
+import SwiftUI
+
+@main
+struct GroupGrubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
