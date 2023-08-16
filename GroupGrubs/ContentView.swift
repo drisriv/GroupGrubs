8c3c46aa7b32702744238dd5802f8ac02b0b7dbd
Drishti Srivastava <drishtis@umich.edu>
2023-08-16 11:51:03 -0400

    started working on welcome screen and created screen 2

--- GroupGrubs/ContentView.swift
+++ GroupGrubs/ContentView.swift
@@ -8,16 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var numPeople=0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-            Text("Lol")
+            Spacer()
+            Text("Welcome")
+                .font(.title)
+            Spacer()
+            HStack{
+                Text("Choose the Number of People in your Group")
+                Picker(selection: $numPeople, label: Text("# of People")) {
+                    Text("2").tag(1)
+                    Text("3").tag(2)
+                    Text("4").tag(3)
+                    Text("5").tag(4)
+                    Text("6").tag(5)
+                    Text("7").tag(6)
+                    Text("8").tag(7)
+                }
+            }
+            Spacer()
+            
         }
-        .padding()
-        .padding()
         .padding()
     }
 }
---
+++ GroupGrubs/foodChoices.swift
@@ -0,0 +1,21 @@
+//
+//  foodChoices.swift
+//  GroupGrubs
+//
+//  Created by Drishti Srivastava on 8/16/23.
+//
+
+import SwiftUI
+
+struct foodChoices: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ContentView().numPeople
+    }
+}
+
+struct foodChoices_Previews: PreviewProvider {
+    static var previews: some View {
+        foodChoices()
+    }
+}
