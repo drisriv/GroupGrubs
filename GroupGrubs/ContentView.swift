f32464651c801698ca1a9d374f41d62be517f7f8
Drishti Srivastava <drishtis@umich.edu>
2023-08-17 10:15:51 -0400

    fixed image issue with logo

--- GroupGrubs/ContentView.swift
+++ GroupGrubs/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-            Image("AppIcon")
+            Image("Logo")
             
             Spacer()
             Text("Welcome")
